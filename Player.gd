5952270d3a2eea2086d7f531807acd674a01dbeb
Sabin Caragea <sabincaragea2005@gmail.com>
2024-02-28 17:04:41 -0500

    we're cooked

--- Player.gd
+++ Player.gd
@@ -11,15 +11,20 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		#velocity.x += 1
+		apply_central_impulse(Vector2(speed * delta, 0))
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		#velocity.x -= 1
+		apply_central_impulse(Vector2(-(speed * delta), 0))
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		#velocity.y += 1
+		apply_central_impulse(Vector2(0, speed * delta))
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		#velocity.y -= 1
+		apply_central_impulse(Vector2(0, -(speed * delta)))
 		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	position += velocity * delta
+		
+	#if velocity.length() > 0:
+		#velocity = velocity.normalized() * speed
+	#position += velocity * delta
 
