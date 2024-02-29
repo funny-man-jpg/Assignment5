a91a21691edb497b7343e4e65e8f0365bee5e079
Sabin Caragea <sabincaragea2005@gmail.com>
2024-02-29 12:44:34 -0500

    core mechanics done

--- main.gd
+++ main.gd
@@ -7,10 +7,12 @@ var threeHouseCount = 0
 var woodCount = 0
 var stoneCount = 0
 var buildable = true
+var idSetter = 0
+var plots = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var plots = get_tree().get_nodes_in_group("Plots")
+	plots = get_tree().get_nodes_in_group("Plots")
 	for i in plots:
 		i.connect("build", plot_built)
 	$Timer.start()
@@ -23,9 +25,14 @@ func _process(delta):
 		buildable = false
 	else:
 		buildable = true
+	for i in plots:
+		i.set_buildable(buildable)
+	#print(buildable)
 
 
 func plot_built(houseType):
+	woodCount -= 3
+	stoneCount -= 1
 	if houseType == 1:
 		regularHouseCount += 1
 	elif houseType == 2:
@@ -39,8 +46,9 @@ func plot_built(houseType):
 
 
 func _on_wood_store_area_entered(area):
-	money -= 1
-	woodCount += 2
+	if money > 0:
+		money -= 1
+		woodCount += 2
 
 
 func _on_timer_timeout():
@@ -48,3 +56,12 @@ func _on_timer_timeout():
 	money += 4 * threeHouseCount
 	money += 2 * doubleHouseCount
 	print(money)
+	print(woodCount)
+	print(stoneCount)
+	print(buildable)
+
+
+func _on_stone_store_area_entered(area):
+	if money > 0:
+		money -= 2
+		stoneCount += 1
--- plot.gd
+++ plot.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 signal build(houseType)
 @export var houseType = 0
+var isBuildable = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +14,11 @@ func _ready():
 func _process(delta):
 	pass
 
+func set_buildable(setBool):
+	isBuildable = setBool
 
 func _on_area_entered(area):
-	build.emit(houseType)
-	$AnimatedSprite2D.set_frame(1)
+	if isBuildable == true:
+		$AnimatedSprite2D.set_frame(1)
+		build.emit(houseType)
 
