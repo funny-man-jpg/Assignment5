24ba0ffccb1c94a1b976f9b0b9d5a6974cd0f678
Sabin Caragea <Sabin Caragea>
2024-02-29 11:30:47 -0500

    new shit

--- main.gd
+++ main.gd
@@ -1,20 +1,50 @@
 extends Node
 
-var money = 0
-var houseCount = 0
+var money = 10
+var regularHouseCount = 0
+var doubleHouseCount = 0
+var threeHouseCount = 0
 var woodCount = 0
 var stoneCount = 0
+var buildable = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var plots = get_tree().get_nodes_in_group("Plots")
+	for i in plots:
+		i.connect("build", plot_built)
+	$Timer.start()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	money += 1 * houseCount
+	if woodCount < 3 and stoneCount < 1:
+		buildable = false
+	else:
+		buildable = true
+
+
+func plot_built(houseType):
+	if houseType == 1:
+		regularHouseCount += 1
+	elif houseType == 2:
+		threeHouseCount += 1
+	elif houseType == 3:
+		doubleHouseCount += 1
+	else:
+		#placeholder for mountains
+		regularHouseCount += 1
+	print(regularHouseCount)
+
+
+func _on_wood_store_area_entered(area):
+	money -= 1
+	woodCount += 2
+
+
+func _on_timer_timeout():
+	money += 1 * regularHouseCount
+	money += 4 * threeHouseCount
+	money += 2 * doubleHouseCount
 	print(money)
-
-
-func _on_plot_build():
-	houseCount += 1
--- store.gd
+++ store.gd
@@ -1,20 +1,10 @@
 extends Area2D
 
-signal build(houseType)
-@export var houseType = 0
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.set_frame(0)
-	add_to_group("Plots")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_area_entered(area):
-	build.emit(houseType)
-	$AnimatedSprite2D.set_frame(1)
-
